7f9977f4df98e65bb4a1e5b92c481de195791dd6
Eric Russell <russell.ericm@gmail.com>
2015-01-24 11:37:49 -0600

    Added physics controls

---
+++ Assets/Scripts/PlayerMv.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMv : MonoBehaviour {
+
+	// Controls
+	static string xAxis = "xAxis";
+	static string yAxis = "yAxis";
+	static string zAxis = "zAxis";
+
+	private string[] axis;
+	private Vector3 axisForce = Vector3.zero;
+
+	private void Start () {
+		axis = new string[] {xAxis, yAxis, zAxis};
+	}
+
+	private void Update () {
+		for(int i = 0; i < 3; i++) {
+			axisForce[i] = Input.GetAxis(axis[i]);
+		}
+	}
+
+	private void FixedUpdate () {
+		rigidbody.AddRelativeForce(axisForce);
+		axisForce = Vector3.zero;
+
+		// print var
+		Debug.Log(rigidbody.velocity);
+	}
+}
