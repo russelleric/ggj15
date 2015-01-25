2bf48c8270cca3260a96c8f010f8f9ea9f8ae7b5
Eric Russell <russell.ericm@gmail.com>
2015-01-25 12:08:13 -0600

    added basic solo lvl

---
+++ Assets/Scripts/GoalMv.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalMv : MonoBehaviour {
+
+
+	private float distance = 100.0f;
+	private float variation = 50.0f;
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log ("hit");
+		float xOffset = Random.Range(-variation, variation);
+		float yOffset = Random.Range(-variation, variation);
+		transform.Translate(new Vector3(xOffset,
+		                                yOffset,
+		                                distance),
+		                    			Space.World);
+	}
+}
---
+++ Assets/Scripts/RotateFx.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateFx : MonoBehaviour {
+
+	public float speed = 90;
+
+	void Update () {
+		// todo add cooler effect
+		transform.Rotate(Vector3.up * speed * Time.deltaTime);
+	}
+}
