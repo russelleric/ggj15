806e194e74823178d64a2700175359cb519d15d5
Eric Russell <russell.ericm@gmail.com>
2015-01-24 20:58:20 -0600

    added distance ui and end game scenes

---
+++ Assets/Scripts/RestartTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartTimer : MonoBehaviour {
+
+	public float TimeUntilRestart = 3.0f;
+	public string LevelName;
+
+	private float LoadedTime;
+
+	private void Start () {
+		LoadedTime = Time.time;
+	}
+
+	private void Update () {
+		if (Time.time > LoadedTime + TimeUntilRestart) {
+			Application.LoadLevel(LevelName);
+		}
+	}
+}
---
+++ Assets/Scripts/ScoreDist.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDist : MonoBehaviour {
+
+	public Color ColorP1;
+	public Color ColorP2;
+	public Transform PlayersMiddlePoint;
+	public Text DistText;
+
+	private float goalP1 = 25;
+	private float goalP2 = -25;
+
+	void Update () {
+		// game over
+		if (PlayersMiddlePoint.position.x >= goalP1 || PlayersMiddlePoint.position.x <= goalP2) {
+			string winLvl = (PlayersMiddlePoint.position.x > 0) ? "winP1" : "winP2";
+			Application.LoadLevel(winLvl);
+		} else {
+			if (PlayersMiddlePoint.position.x > 0) {
+				// player 1 advantage
+				DistText.text = (goalP1 - PlayersMiddlePoint.position.x).ToString("F2");
+				DistText.color = ColorP1;
+			} else {
+				// player 2 advantage
+				DistText.text = (-1.0f * (goalP2 - PlayersMiddlePoint.position.x)).ToString("F2");
+				DistText.color = ColorP2;
+			}
+		}
+	}
+}
