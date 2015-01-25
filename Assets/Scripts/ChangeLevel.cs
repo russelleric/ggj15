3adac6df20378205be6b5fc7ee67860f5a8f1f8d
Eric Russell <russell.ericm@gmail.com>
2015-01-25 11:27:39 -0600

    Added a main menu and control screnes

---
+++ Assets/Scripts/ChangeLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour {
+
+	public void ChangeLvl (string levelName) {
+		Application.LoadLevel(levelName);
+	}
+
+	public void Quit () {
+		Application.Quit();
+	}
+}
--- Assets/Scripts/ScoreDist.cs
+++ Assets/Scripts/ScoreDist.cs
@@ -3,28 +3,33 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class ScoreDist : MonoBehaviour {
-
+	
 	public Color ColorP1;
 	public Color ColorP2;
 	public Transform PlayersMiddlePoint;
 	public Text DistText;
 
+	// settings
+	private string p1WinSceneName = "winP1";
+	private string p2WinSceneName = "winP2";
+	private string floatDisplayFormat = "F2";
+	// goal x positions
 	private float goalP1 = 25;
 	private float goalP2 = -25;
 
 	void Update () {
 		// game over
 		if (PlayersMiddlePoint.position.x >= goalP1 || PlayersMiddlePoint.position.x <= goalP2) {
-			string winLvl = (PlayersMiddlePoint.position.x > 0) ? "winP1" : "winP2";
-			Application.LoadLevel(winLvl);
+			// load correct scene, uses position
+			Application.LoadLevel((PlayersMiddlePoint.position.x > 0) ? p1WinSceneName : p2WinSceneName);
 		} else {
 			if (PlayersMiddlePoint.position.x > 0) {
 				// player 1 advantage
-				DistText.text = (goalP1 - PlayersMiddlePoint.position.x).ToString("F2");
+				DistText.text = (goalP1 - PlayersMiddlePoint.position.x).ToString(floatDisplayFormat);
 				DistText.color = ColorP1;
 			} else {
 				// player 2 advantage
-				DistText.text = (-1.0f * (goalP2 - PlayersMiddlePoint.position.x)).ToString("F2");
+				DistText.text = (-1.0f * (goalP2 - PlayersMiddlePoint.position.x)).ToString(floatDisplayFormat);
 				DistText.color = ColorP2;
 			}
 		}
